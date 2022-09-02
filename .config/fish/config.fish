7ef3532e66d190a60416295c6188509a197dc4b4
Oro <93224879+orowith2os@users.noreply.github.com>
2022-09-02 05:21:41 -0500

    Moves the nested WM stuff to the interactive section

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,15 +2,15 @@ set XDG_DATA_DIRS $HOME/.local/share/flatpak/exports/share:$XDG_DATA_DIRS
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
+    if [ -f /run/.containerenv ]
+	distrobox-host-exec sudo -S chown -R oro:oro /tmp/.X11-unix
+    end
 end
 alias docker=podman
 alias neofetch=hyfetch
 alias flatpak-builder='flatpak run org.flatpak.Builder'
 alias protontricks='flatpak run com.github.Matoking.protontricks'
 #alias 'rpm-ostree install'='rpm-ostree install --apply-live'
-if [ -f /run/.containerenv ]
-	distrobox-host-exec sudo -S chown -R oro:oro /tmp/.X11-unix
-end
 alias toolbox='distrobox-host-exec toolbox'
 alias bundle "toolbox run -c jekyll-toolbox bundle"
 alias jekyll "toolbox run -c jekyll-toolbox jekyll"
